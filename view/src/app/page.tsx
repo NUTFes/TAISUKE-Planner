ccabc0d5b689d97524e689bb6813312b5e2d1f56
akari24320 <24.a.ito.nutfes@gmail.com>
2024-09-25 20:40:20 +0900

    仮EventRemarkの作成とヘッダーのファイル位置の移動

--- view/src/app/page.tsx
+++ view/src/app/page.tsx
@@ -1,8 +1,13 @@
 "use client"
 
-import Header from "./components/layouts/Header/Header"
+import EventName from "./components/common/EventName/EventName"
+import EventRemark from "./components/common/EventRemark/EventRemark"
+
 export default function Home() {
   return (
-      <Header/>
+    <div>
+      <EventName initialText="イベント名を入力"/>
+      <EventRemark initialText="備考を入力"/>
+    </div>
   );
 }
